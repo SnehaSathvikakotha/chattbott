0d299ef662230d723b31c87418f8b5b2ae45e611
SnehaSathvikakotha <111121940+SnehaSathvikakotha@users.noreply.github.com>
2022-10-17 21:55:26 +0530

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\sathvika6-bot-a812-Web-Deploy.pubxml -p:Password=tT9c5leoqp2o06u3qF89Kerrb36dNjvoRFdghgqs8RArzfT3DSMLKqaHhWcE
+
